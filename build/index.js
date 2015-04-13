631004278d9eb64b7b25c9403aecb97b6b6cbee7
andrepolischuk <andre.polischuk@gmail.com>
2015-04-13 12:53:59 +0300

    0.2.0

--- build/index.js
+++ build/index.js
@@ -90,16 +90,36 @@
 'use strict';
 
 var tmg = require('andrepolischuk/tmg@0.2.0');
-var h1 = document.querySelector('h1');
-var time;
 
-tmg(new Date(2016, 0, 1))
+var cyear = document.querySelector('#cyear');
+var nyear = document.querySelector('#nyear');
+var page = document.querySelector('#page');
+
+var year = (new Date()).getFullYear();
+
+tmg(new Date(year, 0, 1))
   .format('d h:m:s')
   .start(function() {
-    time = this.obj();
-    h1.innerHTML = time.d + ' ' + (time.d > 1 ? 'days' : 'day') +
-      ' ' + time.h + ':' + time.m + ':' + time.s;
+    var time = this.obj();
+    cyear.innerHTML = time.d + ' ' + (time.d > 1 ? 'days' : 'day') +
+      ' ' + digits2(time.h) + ':' + digits2(time.m) + ':' + digits2(time.s);
   });
+
+tmg(new Date(year + 1, 0, 1))
+  .format('d h:m:s')
+  .start(function() {
+    var time = this.obj();
+    nyear.innerHTML = time.d + ' ' + (time.d > 1 ? 'days' : 'day') +
+      ' ' + digits2(time.h) + ':' + digits2(time.m) + ':' + digits2(time.s);
+  });
+
+tmg().start(function() {
+  page.innerHTML = this.str();
+});
+
+function digits2(val) {
+  return val < 10 ? '0' + val : val;
+}
 
 }, {"andrepolischuk/tmg@0.2.0":2}],
 2: [function(require, module, exports) {
